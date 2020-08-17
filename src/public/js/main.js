c3e3fe18b727d096b670a8f5c5b45ad7c0d5a4c1
augusto <augustocacers1985@gmail.com>
2020-08-16 21:56:34 -0500

    se agrega nombe user al lado de el msg

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,19 +4,56 @@ $(function(){
     
     const socket=io();
 
-    // obteniendo los elementos de el DOM desde interfaz
+    // !obteniendo los elementos de el DOM desde interfaz
     const $mesaggeForm=$('#message-form');
     const $mesaggeBox=$('#message');
     const $chat=$('#chat');
+    // !obteniendo los elementos de el DOM desde nicknameform
+    const $nickForm= $('#nickForm');
+    const $nickError= $('#nickError');
+    const $nickName= $('#nickName');
 
-    //eventos
+    // !obteniendo los elementos de el DOM desde users
+    const $users=$('#username');
+
+    // !eventos nickform
+    $nickForm.submit(e=>{
+        e.preventDefault()
+        console.log('enviando..');
+        socket.emit('new user',$nickName.val(),function (data){
+            if(data){
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+
+            }else{
+                $nickError.html(`<div class="alert alert-danger">
+                    Ese usaurio ya existe
+                </div>`);
+            }
+            $nickName.val('')
+        });
+    });
+
+    //!eventos app
     $mesaggeForm.submit(e=>{
         e.preventDefault();
         console.log('enviando datos')
         console.log($mesaggeBox.val())
-        //frontend envia a servidor lo de la caja y els ervidor debe escuchar en sockets **
+        //!frontend envia a servidor lo de la caja y els ervidor debe escuchar en sockets **
         socket.emit('send message', $mesaggeBox.val());
         $mesaggeBox.val('')
+    });
+
+    socket.on('new message',function (data){
+        $chat.append('<b>'+ data.nick+'</b>:'+ data.msg+ '</br>')
+    })
+
+    socket.on('usernames',data=>{
+        let html='';
+        for (let i=0; i< data.length;i++){
+            html+=`<p><i class="fas fa-user"></i>${data[i]}</p>`
+        }
+        $users.html(html);
     })
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,10 +1,38 @@
 module.exports= function(io){
+    let nicknames=[];
+    //** */ usuarios cinectados
     io.on('connection',socket=>{
-        console.log('nuevo usuario')
+        console.log('nuevo usuario');
 
         // ** escuchando evnt
+        socket.on('new user',(data,cb)=>{
+            console.log(data)
+            if(nicknames.indexOf(data) != -1){
+                cb(false)
+            }else{
+                cb(true);
+                socket.nickname=data;
+                nicknames.push(socket.nickname);
+                updateNicknames()
+            }
+        });
         socket.on('send message',function(data){
             console.log(data)
+            io.sockets.emit('new message', {
+                msg:data,
+                nick:socket.nickname
+            });
         })
+
+        socket.on('disconnect', data=>{
+            if(!socket.nickname) return;
+            nicknames.splice(nicknames.indexOf(socket.nickname),1);
+            updateNicknames()
+        })
+
+        function updateNicknames (){
+            io.sockets.emit('usernames',nicknames)
+
+        }
     });
 }
